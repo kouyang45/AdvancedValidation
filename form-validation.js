f79b5419936d9caaccc4c0c8c79af98d7fd86d36
kou yang <kyang45@mail.csuchico.edu>
2017-03-09 23:14:25 -0800

    what i got so far
    i have added "else if" to not  allow user to be password and to set the minimum and limit of character. 
    couldn't find out, how to get user to do no spaces and password to not have password as lower and upper case.

--- form-validation.js
+++ form-validation.js
@@ -8,31 +8,55 @@ function validate(){
     document.getElementById("usernameError").classList.add("shown-message");
     //Turn the username items red
     document.getElementById("usernameGroup").classList.add("has-error");
-  }
-  else {
-    //Show message that there is an error with the username...
-    document.getElementById("usernameError").innerHTML="good username.";
-    document.getElementById("usernameError").classList.remove("hidden-message");
-    document.getElementById("usernameError").classList.add("shown-message");
-    //Turn the username items red
-    document.getElementById("usernameGroup").classList.add("has-success");
-    document.getElementById("usernameGroup").classList.remove("has-error");
-  }
-  if (passEntered == "password") {
+    }
+    else {
+      //Show message that there is an error with the username...
+      document.getElementById("usernameError").innerHTML="good username.";
+      document.getElementById("usernameError").classList.remove("hidden-message");
+      document.getElementById("usernameError").classList.add("shown-message");
+      //Turn the username items red
+      document.getElementById("usernameGroup").classList.add("has-success");
+      document.getElementById("usernameGroup").classList.remove("has-error");
+      }
+  if (passEntered =="password") {
     //Show message that there is an error with the password...
     document.getElementById("passwordError").innerHTML="Bad password.";
     document.getElementById("passwordError").classList.remove("hidden-message");
     document.getElementById("passwordError").classList.add("shown-message");
     //Turn the password items red
     document.getElementById("passwordGroup").classList.add("has-error");
-  }
-  else {
-    //Show message that there is an error with the password...
-    document.getElementById("passwordError").innerHTML="good password.";
-    document.getElementById("passwordError").classList.remove("hidden-message");
-    document.getElementById("passwordError").classList.add("shown-message");
-    //Turn the password items red
-    document.getElementById("passwordGroup").classList.add("has-success");
-    document.getElementById("passwordGroup").classList.remove("has-error");
-  }
+    }
+    else if (passEntered.length < 6){
+      //Show message that there is an error with the password...
+      document.getElementById("passwordError").innerHTML="Bad password.";
+      document.getElementById("passwordError").classList.remove("hidden-message");
+      document.getElementById("passwordError").classList.add("shown-message");
+      //Turn the password items red
+      document.getElementById("passwordGroup").classList.add("has-error");
+    }
+    else if (passEntered.length >= 20) {
+      //Show message that there is an error with the username...
+      document.getElementById("passwordError").innerHTML="bad password.";
+      document.getElementById("passwordError").classList.remove("hidden-message");
+      document.getElementById("passwordError").classList.add("shown-message");
+      //Turn the username items red
+      document.getElementById("passwordGroup").classList.add("has-error");
+      }
+    else if (passEntered == userEntered){
+        //Show message that there is an error with the password...
+        document.getElementById("passwordError").innerHTML="Bad password.";
+        document.getElementById("passwordError").classList.remove("hidden-message");
+        document.getElementById("passwordError").classList.add("shown-message");
+        //Turn the password items red
+        document.getElementById("passwordGroup").classList.add("has-error");
+        }
+    else {
+      //Show message that there is an error with the password...
+      document.getElementById("passwordError").innerHTML="good password.";
+      document.getElementById("passwordError").classList.remove("hidden-message");
+      document.getElementById("passwordError").classList.add("shown-message");
+      //Turn the password items red
+      document.getElementById("passwordGroup").classList.add("has-success");
+      document.getElementById("passwordGroup").classList.remove("has-error");
+      }
 }
